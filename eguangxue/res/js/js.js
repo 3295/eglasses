6551950a82e340183efc572f2adda3c60fc81977
coco1106 <1126919170@qq.com>
2016-04-22 10:36:30 +0800

    2

--- eguangxue/res/js/js.js
+++ eguangxue/res/js/js.js
@@ -1,4 +1,27 @@
 var JS={};
+JS.indexpage={
+	'init':function(){
+		if($("#slide_01")){
+			var W = $('body').width();
+			$('.mod_01').width(W);
+			$('.mod_01 img').width(W);
+			var slide_01 = new ScrollPic();
+			slide_01.scrollContId   = "slide_01"; //内容容器ID
+			slide_01.dotListId      = "slide_01_dot";//点列表ID
+			slide_01.dotOnClassName = "selected";
+			slide_01.arrLeftId      = "sl_left"; //左箭头ID
+			slide_01.arrRightId     = "sl_right";//右箭头ID
+			slide_01.frameWidth     = W;
+			slide_01.pageWidth      = W;
+			slide_01.upright        = false;
+			slide_01.speed          = 10;
+			slide_01.space          = 30; 
+			slide_01.initialize(); //初始化
+			var dn = $('.dotModule_new');
+			dn.css('right', (W-dn.width())/2);
+		}
+	}
+};
 JS.prolistpage = {
 	'currpage':1,	//当前页码 默认1
 	'init':function(opt){
@@ -144,7 +167,12 @@ JS.shopadvicepage={
 		})
 	}
 };
-
+JS.listsurepage={
+	'init':function(name,tel,addr){
+		$('.listsure .addbox a').html("<p class='name'>"+name+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+tel+"</p>"+"<p class='add'>"+addr+"</p>");
+		// console.log(name,tel,addr);
+	}
+}
 function count_plus(btn){
 	var inp = btn.prev();
 	var count = parseInt(inp.val())+1;
