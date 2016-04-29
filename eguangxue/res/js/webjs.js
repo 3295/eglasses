6b0cc74b68f982261eb432f545085ec5162cc3f6
coco1106 <1126919170@qq.com>
2016-04-29 09:49:50 +0800

    7

--- eguangxue/res/js/webjs.js
+++ eguangxue/res/js/webjs.js
@@ -33,63 +33,143 @@ JS.appointmentpage={
 		})
 	}
 }
-JS.loginpage={
-	'init':function(){
-	}
-}
 JS.shopaddrpage={
-	'init':function(){
-		$('.userinfo .editor a.radio').click(function(){
-			if ($(this).hasClass('sel')) return false;
-			$(this).addClass('sel').parents('.userinfo').siblings().find('a.radio').removeClass('sel');
- 		})
-		$('.userinfo .editor a.del').click(function(){
-			$(this).parents('.userinfo').remove();
-		})
+	'radio':function(m){
+			if ($(m).hasClass('sel')) return false;
+			$(m).addClass('sel').parents('.userinfo').siblings().find('a.radio').removeClass('sel');
+	},
+	'del':function(m){
+		$(m).parents('.userinfo').remove();
 	}
+	
 }
+// JS.addaddrpage={
+// 	'out':function(){
+// 		$('.gray').hide();
+// 		$('.hidebox').hide();
+// 	},
+// 	'init':function(){
+// 		$('.useraddr .area').click(function(){
+// 			$('.hidebox').show();
+// 			$('.gray').show();
+// 		})
+// 		$('.hidebox .localbox ul.province li').click(function(){
+// 			if ($(this).hasClass('sel')) return false;
+// 			$(this).addClass('sel').siblings().removeClass('sel');
+// 			$(this).parent().css('width','33%');
+// 			$('ul.province li').css('border-right','1px dotted #efeff4');
+// 			$('ul.down').css({'display':'block','width':'67%'})
+// 			$('ul.county').hide();
+// 		})
+// 		$('.hidebox .localbox ul.down li').click(function(){
+// 			if ($(this).hasClass('sel')) return false;
+// 			$(this).addClass('sel').siblings().removeClass('sel');
+// 			$(this).parent().css('width','33%');
+// 			$('ul.down li').css('border-right','1px dotted #efeff4');
+// 			$('ul.county').css({'display':'block','width':'34%'})
+// 		})
+// 		$('.hidebox .localbox ul.county li').click(function(){
+// 			if ($(this).hasClass('sel')) return false;
+// 			$(this).addClass('sel').siblings().removeClass('sel');
+// 		})
+// 		$('.hidebox .btnbox a.sure').click(function(){
+// 			if ($('ul.county li').hasClass('sel')) {
+// 				var result='';
+// 			var aaa=$('.hidebox .localbox ul li.sel a');
+// 			$.each(aaa,function(i,e){
+// 				result+=$(e).html()+' ';
+// 			})
+// 			$('.useraddr .area span').html(result);
+// 			JS.addaddrpage.out();
+// 		}else{
+// 			return false;
+// 		}
+			
+// 		})
+// 	}
+// }
 JS.addaddrpage={
 	'out':function(){
 		$('.gray').hide();
 		$('.hidebox').hide();
 	},
+	'province':function(M){
+		if ($(M).parent("li").hasClass('sel')) return false;
+		$(M).parent("li").addClass("sel").siblings().removeClass("sel");
+		
+		
+		$(M).parents("ul.province").css('width','33%');
+		$('ul.province li').css('border-right','1px dotted #efeff4');
+		$('ul.down').css({'display':'block','width':'67%'})
+		$('ul.county').hide();
+	},
+	'city':function(M){
+		if ($(M).parent().hasClass('sel')) return false;
+		$(M).parent().addClass('sel').siblings().removeClass('sel');
+		$(M).css("border-bottom","2px solid #b81c22");
+		$(M).parents("li").siblings().find("a").css('border-bottom',"none");
+		$(M).parents("ul.down").css('width','33%');
+		$('ul.down li').css('border-right','1px dotted #efeff4');
+		$('ul.county').css({'display':'block','width':'34%'})
+	},
+	'county':function(M){
+		if ($(M).hasClass('sel')) return false;
+		$(M).addClass('sel').siblings().removeClass('sel');
+	},
 	'init':function(){
 		$('.useraddr .area').click(function(){
 			$('.hidebox').show();
 			$('.gray').show();
-		})
-		$('.hidebox .localbox ul.province li').click(function(){
-			if ($(this).hasClass('sel')) return false;
-			$(this).addClass('sel').siblings().removeClass('sel');
-			$(this).parent().css('width','33%');
-			$('ul.province li').css('border-right','1px dotted #efeff4');
-			$('ul.down').css({'display':'block','width':'67%'})
-			$('ul.county').hide();
-		})
-		$('.hidebox .localbox ul.down li').click(function(){
-			if ($(this).hasClass('sel')) return false;
-			$(this).addClass('sel').siblings().removeClass('sel');
-			$(this).parent().css('width','33%');
-			$('ul.down li').css('border-right','1px dotted #efeff4');
-			$('ul.county').css({'display':'block','width':'34%'})
-		})
-		$('.hidebox .localbox ul.county li').click(function(){
-			if ($(this).hasClass('sel')) return false;
-			$(this).addClass('sel').siblings().removeClass('sel');
-		})
+		});	
 		$('.hidebox .btnbox a.sure').click(function(){
+			
 			if ($('ul.county li').hasClass('sel')) {
 				var result='';
+				var code='';
+				
 			var aaa=$('.hidebox .localbox ul li.sel a');
 			$.each(aaa,function(i,e){
 				result+=$(e).html()+' ';
-			})
+				code=$(e).attr("tag");
+			});
+			$(".area input").val(code);
 			$('.useraddr .area span').html(result);
 			JS.addaddrpage.out();
 		}else{
 			return false;
 		}
 			
-		})
+		});
 	}
+	
+};
+JS.mycollection={
+	'init':function(){
+		
+	},
+	'show':function(){
+		$('.mycollectionpage ul li .grabox').width($('.mycollectionpage ul li a').width());
+		$('.mycollectionpage ul li .grabox').height($('.mycollectionpage ul li a').height());
+		$('.mycollectionpage ul li .grabox').show();
+	},
+	'del':function(m){
+		$(m).parents('li').remove();
+	}
+	
+};
+function promp(tiph,tipc){
+	var prompbox="<div class='prompbox' style='z-index:999;width:70%;height:24%;position:fixed;left:15%;top:38%;background:#ffffff;border:1px solid #9a9a9a;'></div>";
+	var gray="<div class='gray' style='z-index:998;width:100%;height:100%;position:fixed;left:0px;top:0px;background:#000000;opacity:0.8;'></div>";
+	var tipcont="<div class='cont' style='margin:50px 10px;font-size:12px;color:#333333;line-height:18px;overflow:hidden;'>"+tipc+"</div>"
+	var tiphead="<div class='tiphead' style='position:absolute;left:0px;top:0px;width:100%;height:40px;border-bottom:1px solid #efeff4'><p style='padding:0px 10px;font-size:12px;color:#000000;line-height:40px;text-align:center;font-weight:bold;'>"+tiph+"</p></div>"
+	var tipsurebtn="<div class='surebox' style='width:100%;height:40px;background:#efeff4;position:absolute;bottom:0px;left:0px;'><a onclick='tiphide()' style='display:block;width:60px;height:30px;margin:5px auto;background:#b81c22;color:#ffffff;text-align:center;line-height:30px;border-radius:5px;'>确定</a></div>"
+	$('body').append(gray);
+	$('body').append(prompbox);
+	$('body .prompbox').append(tiphead);
+	$('body .prompbox').append(tipsurebtn);
+	$('body .prompbox').append(tipcont);
+}
+function tiphide(){
+		$('.prompbox').hide();
+		$('.gray').hide();
 }
